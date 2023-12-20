1af337b25b82c403282889aac58ef4398dfacb7e
saikiran <sai468282@gmail.com>
2023-12-20 18:47:29 +0530

    X cmmits the code

--- src/test/java/Datix_Smoketest/Hooks.java
+++ src/test/java/Datix_Smoketest/Hooks.java
@@ -3,10 +3,7 @@ package Datix_Smoketest;
 public class Hooks {
 
 	public static void main(String[] args) {
-System.out.println("this is hooks class");
-System.out.println("this is hooks file by saikiran");
-System.out.println("this is hooks file by datix X");
-System.out.println("this is hooks file by saikiran datixX");
+System.out.println("this is x devloper");
 
 	}
 
