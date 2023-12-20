73bebfee1cd1aeee7ec4effdcf3b31da9c33ba01
saikiran <sai468282@gmail.com>
2023-12-20 15:28:01 +0530

    changed by datix x

--- src/test/java/Datix_Smoketest/Hooks.java
+++ src/test/java/Datix_Smoketest/Hooks.java
@@ -5,6 +5,9 @@ public class Hooks {
 	public static void main(String[] args) {
 System.out.println("this is hooks class");
 System.out.println("this is hooks file by saikiran");
+System.out.println("this is hooks file by datix X");
+System.out.println("this is hooks file by saikiran datixX");
+
 	}
 
 }
