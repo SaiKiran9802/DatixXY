46cb55204e8ebf2e5610cc97e4e306a4c86484c9
saikiran <sai468282@gmail.com>
2023-12-20 19:42:50 +0530

    devlopbranch hooks files chages by Y

--- src/test/java/Datix_Smoketest/Hooks.java
+++ src/test/java/Datix_Smoketest/Hooks.java
@@ -3,8 +3,7 @@ package Datix_Smoketest;
 public class Hooks {
 
 	public static void main(String[] args) {
-System.out.println("sai");
-System.out.println("Kiran");
+System.out.println("developbranch from Y");
 
 	}
 
