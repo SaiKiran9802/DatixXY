58cd5c49f5b947062258aa9e2b0ac73f2c9281e1
saikiran <sai468282@gmail.com>
2024-01-03 21:21:55 +0530

    commit by saikiran

--- src/test/java/Datix_Smoketest/Hooks.java
+++ src/test/java/Datix_Smoketest/Hooks.java
@@ -3,7 +3,7 @@ package Datix_Smoketest;
 public class Hooks {
 
 	public static void main(String[] args) {
-System.out.println("developbranch from Y");
+System.out.println("deveop branch from za");
 
 	}
 
