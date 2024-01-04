ae091f4f5b181ae26b76671f1711ad44ae695b71
saikiran <sai468282@gmail.com>
2024-01-04 22:07:15 +0530

    changes on 4th

--- src/test/java/Datix_Smoketest/Hooks.java
+++ src/test/java/Datix_Smoketest/Hooks.java
@@ -3,7 +3,7 @@ package Datix_Smoketest;
 public class Hooks {
 
 	public static void main(String[] args) {
-System.out.println("this is x devloper");
+System.out.println("this is x devloper ");
 
 	}
 
